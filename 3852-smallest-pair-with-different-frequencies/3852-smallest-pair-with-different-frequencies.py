9f20e18cfa4ff8d43a3bb11543d4aa0c0e67155f
Sakshi Singh <sukku0996@gmail.com>
2026-03-03 03:15:49 +0530

    Time: 3 ms (80.95%), Space: 19.2 MB (90.06%) - LeetHub

--- 3852-smallest-pair-with-different-frequencies/3852-smallest-pair-with-different-frequencies.py
+++ 3852-smallest-pair-with-different-frequencies/3852-smallest-pair-with-different-frequencies.py
@@ -4,10 +4,10 @@ class Solution:
         for ch in nums:
             freq[ch]=freq.get(ch,0)+1
 
-        nums.sort()
-        for i in range(1,len(nums)):
-            if freq[nums[0]]!=freq[nums[i]]:
-                return [nums[0],nums[i]]
+        unique=sorted(freq.keys())
+        for i in range(1,len(unique)):
+            if freq[unique[0]]!=freq[unique[i]]:
+                return [unique[0],unique[i]]
 
 
         return [-1,-1]        
