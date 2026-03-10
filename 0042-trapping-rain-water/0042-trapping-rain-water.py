4637a87975c04fbc9727d4e5a78dfc2f6afa09f7
Sakshi Singh <sukku0996@gmail.com>
2026-03-11 01:21:15 +0530

    Time: 15 ms (29%), Space: 21.2 MB (44.37%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,37 +1,18 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        n = len(height)
-        
-        # Initialize two pointers at both ends of the list
-        left, right = 0, n - 1
-        
-        # Variables to track the maximum height to the left and right
-        max_left = 0
-        max_right = 0
-        
-        # Variable to store total trapped water
-        total_water = 0
-        
-        # Iterate until left pointer meets right pointer
-        while left <= right:
-            # If left bar is smaller or equal to right bar
-            if height[left] <= height[right]:
-                # If current left bar is higher than max_left, update max_left
-                if height[left] >= max_left:
-                    max_left = height[left]
-                else:
-                    # Water trapped on left is difference between max_left and current height
-                    total_water += max_left - height[left]
-                left += 1  # Move left pointer to the right
-            else:
-                # If current right bar is higher than max_right, update max_right
-                if height[right] >= max_right:
-                    max_right = height[right]
-                else:
-                    # Water trapped on right is difference between max_right and current height
-                    total_water += max_right - height[right]
-                right -= 1  # Move right pointer to the left
-        
-        # Return total trapped water
-        return total_water
+        n=len(height)
+        left_max=[0]*n
+        right_max=[0]*n
+        left_max[0]=height[0]
+        right_max[n-1]=height[n-1]
+        for i in range(1,n):
+            left_max[i]=max(left_max[i-1],height[i])
+        for i in range(n-2,-1,-1):
+            right_max[i]=max(right_max[i+1],height[i])
+
+        total=0
+        for i in range(n):
+            if left_max[i]>height[i] and right_max[i]>height[i]:
+                total+=min(left_max[i],right_max[i])-height[i]
+        return total                
         
